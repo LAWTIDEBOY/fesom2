3a4aa93cc414eff07d001667b5cf342d4dda58e9
LAWTIDEBOY <38709425+LAWTIDEBOY@users.noreply.github.com>
2021-09-10 14:48:30 -0400

    Update recom_modules.F90

--- src/int_recom/recom_modules.F90
+++ src/int_recom/recom_modules.F90
@@ -338,8 +338,8 @@ Module REcoM_declarations
   Real(kind=8)  :: arrFuncZoo2           ! []    Temperature function for krill     
 !  endif
   Real(kind=8)  :: reminSiT
-  Real(kind=8)  :: reminSiC
-  Real(kind=8)  :: reminSiN
+  Real(kind=8)  :: reminCT
+  Real(kind=8)  :: reminNT
 !-------------------------------------------------------------------------------
 ! Quotas
   Real(kind=8)  :: quota, quota_dia           ! [mmol N/mmol C]  Quota between phytoplankton N and C
